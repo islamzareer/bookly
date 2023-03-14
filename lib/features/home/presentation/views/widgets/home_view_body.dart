d051ca35194c1d14f05035bc659a7188a1ccbbac
islam zareer <187062@ppu.edu.ps>
2023-03-14 13:57:49 +0200

    add feture ListView to homeBody

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
 import 'custom_listview_item.dart';
+import 'featured_books_listview.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -8,7 +9,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(children: const [CustomAppBar(), CustomListViewItem()]),
+      child: Column(children: const [CustomAppBar(), FeaturedBooksListView()]),
     );
   }
 }
