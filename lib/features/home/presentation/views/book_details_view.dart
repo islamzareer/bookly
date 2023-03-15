c7fe4477a1409167f05087993c41617ea3dd3536
islam zareer <187062@ppu.edu.ps>
2023-03-15 15:03:30 +0200

    create book details view

---
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body:  BookDetailsViewBody());
+  }
+}
