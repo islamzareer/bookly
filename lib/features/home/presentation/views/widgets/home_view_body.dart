eaf08211e1dd0f1ac2328e592879381f1f0f9c9f
islam zareer <187062@ppu.edu.ps>
2023-03-23 11:39:56 +0200

    create Featured Books cubit states

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'best_seller_list_view.dart';
+import 'newest_books_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_listview.dart';
 
@@ -35,7 +35,7 @@ class HomeViewBody extends StatelessWidget {
         const SliverFillRemaining(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 30),
-            child: BestSellerListView(),
+            child: NewestBooksListView(),
           ),
         )
       ],
--- lib/features/home/presentation/views/widgets/newest_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/newest_books_list_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'book_list_view_item.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class NewestBooksListView extends StatelessWidget {
+  const NewestBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
